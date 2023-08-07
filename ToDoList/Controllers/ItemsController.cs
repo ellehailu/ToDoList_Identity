0280a105390d19184595f252c11a80f36688782c
Student <student@epicodus-4.hsd1.or.comcast.net>
2023-08-07 13:52:36 -0700

    completed project

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -4,34 +4,45 @@ using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace ToDoList.Controllers
 {
+  [Authorize]
   public class ItemsController : Controller
   {
     private readonly ToDoListContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public ItemsController(ToDoListContext db)
+    public ItemsController(UserManager<ApplicationUser> userManager, ToDoListContext db)
     {
+      _userManager = userManager;
       _db = db;
     }
 
-    public ActionResult Index()
+    public async ActionResult Index()
     {
-      List<Item> model = _db.Items
-                            .Include(item => item.Category)
-                            .ToList();
-      return View(model);
+      string userId = userId.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      ApplicationUser currentUser = await _userManager.FirndByAsync(userId);
+      ToDoList<ItemsController> userItems = _db.Items
+        .Where(entry => entry.User.Id == currentUser.Id)
+        .Include(item => item.Category)
+        .ToList();
+      return View(userItems);
     }
 
     public ActionResult Create()
     {
+  
       ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View();
     }
 
     [HttpPost]
-    public ActionResult Create(Item item)
+    public async ActionResult Create(Item item, int CategoryId)
     {
       if (!ModelState.IsValid)
       {
@@ -40,6 +51,9 @@ namespace ToDoList.Controllers
       }
       else
       {
+        string userId = userId.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        ApplicationUser currentUser = await _userManager.FindByAsync(userId);
+        item.User = currentUser;
         _db.Items.Add(item);
         _db.SaveChanges();
         return RedirectToAction("Index");
